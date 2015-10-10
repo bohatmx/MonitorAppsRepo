43dc99e5aa9523f14e99e99c18df41b35e14a50a
Aubrey Malabie <malengatiger@gmail.com>
2015-10-10 09:54:55 +0200

    Removed directory3

--- MonitorPlatform005/MonitorLibrary/src/main/java/com/boha/monitor/library/fragments/PageFragment.java
+++
@@ -1,8 +0,0 @@
-package com.boha.monitor.library.fragments;
-
-/**
- * Created by aubreyM on 2014/05/20.
- */
-public interface PageFragment {
-    public void animateHeroHeight();
-}
--- MonitorPlatform005/MonitorLibrary/src/main/java/com/boha/monitor/library/util/PersonInterface.java
+++
@@ -1,7 +0,0 @@
-package com.boha.monitor.library.util;
-
-/**
- * Created by aubreyM on 2014/04/10.
- */
-public interface PersonInterface {
-}
